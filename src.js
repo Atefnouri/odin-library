436083f42b0d7256a1739e41575c04d2fd4fff61
Atef Nouri <atef.nouri@gmail.com>
2026-08-20 10:12:19 +0100

    Implement code changes to improve UI using bootstrap

--- src.js
+++ src.js
@@ -1,5 +1,5 @@
 //init
-const tableBody = document.getElementById("tablebody");
+const tableBody = document.getElementById("bookGrid");
 const myButton = document.getElementById("addbook");
 let bookTitle = document.getElementById("bookTitle");
 let bookAuthor = document.getElementById("bookAuthor");
@@ -7,6 +7,12 @@ let bookPgnumber = document.getElementById("bookPgnumber");
 let bookYear = document.getElementById("bookYear");
 let isItRead = document.getElementById("isItRead");
 let myLibrary = [];
+
+
+//render the footer text 
+let footer =  document.getElementById("footerContent");
+footer.innerHTML =`© ${ new Date().getFullYear()} Atef Nouri. All rights reserved.`;
+
 
 //constructor
 function Book(title,author,page,year,isRead) {
@@ -38,12 +44,13 @@ function addBookToLibrary(title,author,page,year,isRead) {
 let newBook = new Book(title,author,page,year,isRead);
  myLibrary.push(newBook);
  console.log(myLibrary);
-displayLibrary(); 
+displayLibrary();
+//alert("Book was added succsuflly") 
 }
 
 //enable addbutton functionaly
 myButton.addEventListener('click',(event) =>{
-event.preventDefault();-
+event.preventDefault();
 addBookToLibrary(bookTitle.value,bookAuthor.value,bookPgnumber.value,bookYear.value,isItRead.checked);
 });
 
@@ -89,28 +96,44 @@ function displayLibrary(){
 
    for(let i=0;i<myLibrary.length;i++){
 
-    if(myLibrary[i].isRead){
-    
-        innerContent+=`<tr>
-    <td>${myLibrary[i].title}</td>
-    <td>${myLibrary[i].author}</td>
-    <td>${myLibrary[i].page}</td>
-    <td>${myLibrary[i].year}</td>
-    <td><input type="checkbox" onclick="updateStatus('${myLibrary[i].id}')"  checked> </td>
-    <td><button onclick="deleteBook('${myLibrary[i].id}')">Delete</button></td>
-    </tr>`
-    }
-    else {
-    innerContent+=`<tr>
-    <td>${myLibrary[i].title}</td>
-    <td>${myLibrary[i].author}</td>
-    <td>${myLibrary[i].page}</td>
-    <td>${myLibrary[i].year}</td>
-    <td><input onclick="updateStatus('${myLibrary[i].id}')" type="checkbox"></td>
-    <td><button onclick="deleteBook('${myLibrary[i].id}')">Delete</button></td>
-    </tr>`
 
-    }
+  innerContent+=` <div class="col-md-4">
+							<div class="card border-0 rounded-0 shadow mb-4 mb-md-0">
+								<img alt="" class="img-fluid" src="https://freefrontend.dev/assets/rectangle-wide.png">
+								<div class="card-body text-center mt-3">
+									<h5>${myLibrary[i].title}</h5>
+									<div class="text-muted">
+										By <b>${myLibrary[i].author} </b>
+									</div>
+									<!--<p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut.</p>-->
+								</div>
+							</div>
+						</div>`;
+
+    // if(myLibrary[i].isRead){
+    
+    //     innerContent+=`<tr>
+    // <td>${myLibrary[i].title}</td>
+    // <td>${myLibrary[i].author}</td>
+    // <td>${myLibrary[i].page}</td>
+    // <td>${myLibrary[i].year}</td>
+    // <td><input type="checkbox" onclick="updateStatus('${myLibrary[i].id}')"  checked> </td>
+    // <td><button onclick="deleteBook('${myLibrary[i].id}')">Delete</button></td>
+    // </tr>`
+
+
+    // }
+    // else {
+    // innerContent+=`<tr>
+    // <td>${myLibrary[i].title}</td>
+    // <td>${myLibrary[i].author}</td>
+    // <td>${myLibrary[i].page}</td>
+    // <td>${myLibrary[i].year}</td>
+    // <td><input onclick="updateStatus('${myLibrary[i].id}')" type="checkbox"></td>
+    // <td><button onclick="deleteBook('${myLibrary[i].id}')">Delete</button></td>
+    // </tr>`
+
+    // }
     
 
 
@@ -118,6 +141,24 @@ function displayLibrary(){
 }
 
 }
+
+
+
+//populate the libaray 
+// Sample real-world book data matching your function signature:
+// addBookToLibrary(title, author, pages, year, isRead);
+
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, 1960, true);
+addBookToLibrary("1984", "George Orwell", 328, 1949, true);
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, 1925, false);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, 1937, true);
+
+
+
+
+
+
+
 
 
 
